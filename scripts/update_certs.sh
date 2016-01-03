8ca5b72936674a342261ecb57a3169d8b7d3db61
Samuel Hug <samuel.b.hug@gmail.com>
2016-01-03 12:34:41 -0800

    Update docker images in update script

--- scripts/update_certs.sh
+++ scripts/update_certs.sh
@@ -3,9 +3,11 @@
 DOMAIN="sa.muelh.ug"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+CONTAINER="quay.io/letsencrypt/letsencrypt:latest"
 
+docker pull $CONTAINER
 docker run -it --rm --name letsencrypt \
 	-v "$DIR/www:/var/www" \
 	-v "$DIR/keys:/etc/letsencrypt" \
-	quay.io/letsencrypt/letsencrypt:latest --renew certonly --webroot -w /var/www -d $DOMAIN
+	$CONTAINER --renew certonly --webroot -w /var/www -d $DOMAIN
 
--- update.sh
+++ update.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+pushd "$DIR"
+
+docker pull nginx:latest
+docker-compose stop
+docker-compose rm -f
+docker-compose up -d
+
 ./scripts/update_dns.sh
 ./scripts/update_certs.sh
+
+popd
