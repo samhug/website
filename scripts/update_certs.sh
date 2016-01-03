41c92c34d250a9bf0efee977a13c7ff839354eb9
Samuel Hug <samuel.b.hug@gmail.com>
2016-01-02 17:18:06 -0800

    Make update_certs script renew certificate

--- scripts/update_certs.sh
+++ scripts/update_certs.sh
@@ -7,5 +7,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 docker run -it --rm --name letsencrypt \
 	-v "$DIR/www:/var/www" \
 	-v "$DIR/keys:/etc/letsencrypt" \
-	quay.io/letsencrypt/letsencrypt:latest certonly --webroot -w /var/www -d $DOMAIN
+	quay.io/letsencrypt/letsencrypt:latest --renew certonly --webroot -w /var/www -d $DOMAIN
 
