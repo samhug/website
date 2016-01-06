52d1584f9a0ec62dc73da9e548a904bf81b8e6ee
Samuel Hug <samuel.b.hug@gmail.com>
2016-01-05 16:43:21 -0800

    Document update script

--- update.sh
+++ update.sh
@@ -4,12 +4,21 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 pushd "$DIR"
 
-docker pull nginx:latest
+# Update docker images to latest version
+docker-compose pull
+
+# Stop server and rebuild from scratch
 docker-compose stop
 docker-compose rm -f
 docker-compose up -d
 
+# Update DNS forwardings through HE
 ./scripts/update_dns.sh
+
+# Renew SSL certificates through letsencrypt
 ./scripts/update_certs.sh
+
+# Restart to make nginx use the latest certs
+docker-compose restart
 
 popd
