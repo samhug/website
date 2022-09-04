2f87dafd7dc1d435f787c91ef2a3149ae0f8eed6
Sam Hug <s@m-h.ug>
2022-09-04 10:56:54 -0700

    .

--- app/src/config.rs
+++ app/src/config.rs
@@ -1,6 +1,5 @@
 use figment::{
     providers::{Env, Serialized},
-    util::map,
     Figment,
 };
 use serde::{Deserialize, Serialize};
@@ -10,25 +9,32 @@ use std::{net::SocketAddr, path::PathBuf};
 pub struct Config {
     pub static_files_dir: PathBuf,
     pub listen_addr: SocketAddr,
-    pub http_host: Option<String>,
+    pub https_redirect: bool,
+    pub host_redirect: Option<String>,
 }
 
 impl Config {
     pub fn from_default_sources() -> Result<Self, figment::Error> {
-        let mut defaults = map![
-            "listen_addr" => "127.0.0.1:8000",
-        ];
+        let mut figment = Figment::new()
+            .join(Serialized::default("listen_addr", "127.0.0.1:8000"))
+            .join(Serialized::default("https_redirect", false))
+            .join(Serialized::default("host_redirect", Option::<String>::None))
+            // #
+            ;
 
+        // For convenience, when compiled using the debug profile,
+        // serve static files from the project dir by default
         #[cfg(debug_assertions)]
         {
-            defaults.insert(
+            figment = figment.join(Serialized::default(
                 "static_files_dir",
                 concat!(env!("CARGO_MANIFEST_DIR"), "/static"),
-            );
+            ));
         }
 
-        Figment::from(Serialized::from(&defaults, "default"))
-            .merge(Env::prefixed("APP_"))
-            .extract()
+        // Add configuration from environment variables
+        figment = figment.merge(Env::prefixed("APP_"));
+
+        figment.extract()
     }
 }
